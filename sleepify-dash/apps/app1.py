573eb22f9c97bd0b9b4d44837e71f713dd830a55
Moritz Rocholl <moritz.rocholl@yahoo.de>
2018-11-24 15:40:03 +0100

    interim

--- sleepify-dash/apps/app1.py
+++ sleepify-dash/apps/app1.py
@@ -1,33 +1,65 @@
 import dash_core_components as dcc
-import plotly.graph_objs as go
 import dash_html_components as html
+import plotly.graph_objs as go
 from dash.dependencies import Input, Output
+import pandas as pd
 
 from app import app
 
-layout = html.Div([
+categories = ["Coffee", "Entertainment", "Weather", "Mood", "Activity"]
+days = 7
 
-    html.H3('App 1'),
-    dcc.Dropdown(
-        id='app-1-dropdown',
-        options=[
-            {'label': 'App 1 - {}'.format(i), 'value': i} for i in [
-                'NYC', 'MTL', 'LA'
+layout = html.Div(
+    [
+        dcc.Link('Go to App 2', href='/apps/app2'),
+        html.Div(
+            [
+                html.H3("Coffee"),
+                dcc.Dropdown(
+                    id="categories",
+                    options=[
+                        {'label': f"{category}", 'value': category}
+                        for category in categories
+                    ]
+                ),
+                *(dcc.Graph(id=f"coffee-{i}") for i in range(days)),
             ]
-        ]
-    ),
-    html.Div(id='app-1-display-value'),
-    dcc.Link('Go to App 2', href='/apps/app2'),
-    html.Div([html.H3("Some time circle")])],
-
-
-
-
+        ),
+    ]
 )
 
 
 @app.callback(
-    Output('app-1-display-value', 'children'),
-    [Input('app-1-dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+        Output("coffee-0", "figure"),
+        [Input("categories", "value")]
+        )
+def update_coffee_graph(value):
+    # df = pd.read_csv("...")
+    # y_vals = pd.Series(range(100))
+
+    trace = go.Scatterpolar(
+        r=list(range(24)),
+        theta=list(range(1, 25)),
+    )
+
+    layout = go.Layout(
+        autosize=True,
+        width=275,
+        margin=go.Margin(
+            t=10,
+            b=10,
+            r=30,
+            l=40
+        ),
+        polar=dict(
+            radialaxis=dict(
+                visible=False
+                ),
+            angularaxis=dict(
+                showline=False,
+                tickcolor='white',
+            )
+        ),
+        showlegend=False,
+    )
+    return go.Figure(data=[trace], layout=layout)
