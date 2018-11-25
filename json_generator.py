ad334ecb0c5075b344ee521485b50c746cad3636
SvenUnleashed <sven.aan.het.werk@gmail.com>
2018-11-25 13:34:27 +0100

    Improved performance json_generator.py and added more comments

--- json_generator.py
+++ json_generator.py
@@ -28,7 +28,12 @@ with codecs.open('Data/daylio_data.csv', 'rU', 'utf-16') as csvfile:
     weather_dict = {}
     weather_history_list = {}
 
+    with open('Data/weather_history_list.json') as weather_json:
+        weather_history_list = json.load(weather_json)
+
     for row in reader:
+        hasNew = False
+
         # Retrieve day number from the date column (ex: 1 from 1-Sep and 30 from 30-Sep)
         day = row['date'][:row['date'].find("-")]
 
@@ -71,27 +76,29 @@ with codecs.open('Data/daylio_data.csv', 'rU', 'utf-16') as csvfile:
         hour_root = day_root[hour]
         hour_root['mood'] = row['mood']
 
+        # Activities in the daylio file are exported as a list separated by " | "
+        # f.e.: Work (employment) | Networking | Reading | Programming / Server | Miracle morning | Girlfriend time
         for activity in row['activities'].split(" | "):
             hour_root['activities'][activity.lower()] = True
-
-        # ICON URL: https://icons.wxug.com/i/c/a/<ICON>.gif
 
         timestamp_yyyymmdd = row['year'] + month_MM + day
         timestamp_yyyymmddhh = row['year'] + month_MM + day + hour
 
+        # Already entry for this YYYYMMDDHH processed before? So for example Daylio entry for same day and same hour as already processed, but half hour later
         if timestamp_yyyymmddhh not in weather_dict:
             try:
-                with open('Data/weather_history_list.json') as weather_json:
-                    weather_history_list = json.load(weather_json)
-
+                # Already retrieved weather data in the past for this date? If not, retrieve it from the Weather Underground API and store it for later in the history list JSON file
                 if timestamp_yyyymmdd not in weather_history_list:
                     with urllib.request.urlopen("https://api.wunderground.com/api/41e8f2f12e558b69/history_" + timestamp_yyyymmdd + "/q/ch/Basel.json") as url:
                         print("Called API for date " + timestamp_yyyymmdd)
                         weather_data = json.loads(url.read().decode())
                         weather_history_list[timestamp_yyyymmdd] = weather_data
+                        hasNew = True
                 else:
                     weather_data = weather_history_list[timestamp_yyyymmdd]
+                    hasNew = False
 
+                # Add retrieved data in the dict for later use
                 for observation in weather_data['history']['observations']:
                     timestamp_yyyymmddhh = observation['date']['year'] + observation['date']['mon'] + observation['date']['mday'] + observation['date']['hour']
                     weather_dict[timestamp_yyyymmddhh] = {'temperature_celcius': observation['tempm'], 'conds': observation['conds'], 'icon': observation['icon']}
@@ -101,20 +108,24 @@ with codecs.open('Data/daylio_data.csv', 'rU', 'utf-16') as csvfile:
                 print("API Error - Key Missing")
                 pass
             finally:
-                with open('Data/weather_history_list.json','w') as weather_json:
-                    weather_json.write(json.dumps(weather_history_list, sort_keys=True))
+                # Store newly retrieved data in the history list JSON
+                if hasNew == True:
+                    with open('Data/weather_history_list.json','w') as weather_json:
+                        weather_json.write(json.dumps(weather_history_list, sort_keys=True))
                 pass
 
             
         timestamp_yyyymmddhh = row['year'] + month_MM + day + hour
         hour_root['weather'] = weather_dict[timestamp_yyyymmddhh]['conds']
         hour_root['temperature_celcius'] = weather_dict[timestamp_yyyymmddhh]['temperature_celcius']
+        # ICON URL: https://icons.wxug.com/i/c/a/<ICON>.gif
         hour_root['icon'] = weather_dict[timestamp_yyyymmddhh]['icon']
 
 
     json_data = json.dumps(data, sort_keys=True)
 
     print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
     with open('Data/sleepify_data.json','w') as sleepify_json:
         sleepify_json.write(json_data)
+        print("File Data/sleepify_data.json created/updated!")
+    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
